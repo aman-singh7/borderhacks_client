5ef72d319d77f9633d7126c4ec1275f82b323019
aman-singh7 <aman2@me.iitr.ac.in>
2021-09-26 01:58:15 +0530

    feat: implement `Login` UI

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,24 @@
+import 'package:borderhacks_client/services/local_storage_service.dart';
 import 'package:borderhacks_client/viewmodels/home_viewmodel.dart';
+import 'package:borderhacks_client/viewmodels/login_viewmodel.dart';
+import 'package:borderhacks_client/viewmodels/signup_viewmodel.dart';
+import 'package:borderhacks_client/viewmodels/startup_viewmodel.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
 
 Future<void> setupLocator() async {
-  locator.registerFactory<HomeViewModel>(() => HomeViewModel());
+  var localStorage = await LocalStorageService.getInstance();
+  locator.registerSingleton<LocalStorageService>(localStorage);
+  var _firebaseAuth = FirebaseAuth.instance;
+  locator.registerSingleton<FirebaseAuth>(_firebaseAuth);
+  var _firebaseFirestore = FirebaseFirestore.instance;
+  locator.registerSingleton<FirebaseFirestore>(_firebaseFirestore);
+
+  locator.registerFactory(() => StartUpViewModel());
+  locator.registerFactory(() => LoginViewModel());
+  locator.registerFactory(() => SignupViewModel());
+  locator.registerFactory(() => HomeViewModel());
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:borderhacks_client/locator.dart';
 import 'package:borderhacks_client/ui/view/home_view.dart';
+import 'package:borderhacks_client/ui/view/login_view.dart';
+import 'package:borderhacks_client/ui/view/signup_view.dart';
+import 'package:borderhacks_client/ui/view/startup_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await setupLocator();
   runApp(const MyApp());
 }
@@ -13,12 +19,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeView(),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => const StartUpView()),
+        GetPage(name: '/login', page: () => const LoginView()),
+        GetPage(name: '/signup', page: () => const SignupView()),
+        GetPage(name: '/home', page: () => const HomeView()),
+      ],
+      home: const StartUpView(),
     );
   }
 }
