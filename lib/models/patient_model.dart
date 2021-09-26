5aa80a06b82debe24ddc94f38a2048205a8b659a
Aman Yadav <aman_y@ch.iitr.ac.in>
2021-09-26 20:30:19 +0530

    feat:add myappointments screen

---
+++ lib/models/patient_model.dart
@@ -0,0 +1,10 @@
+class Patient {
+  String clinicAddress, doctorName, patientName, patientAge;
+
+  Patient({
+    required this.clinicAddress,
+    required this.doctorName,
+    required this.patientName,
+    required this.patientAge,
+  });
+}
--- lib/ui/view/myappointment_view.dart
+++ lib/ui/view/myappointment_view.dart
@@ -1,10 +1,41 @@
+import 'base_view.dart';
+import 'package:borderhacks_client/ui/components/appointments_tile.dart';
+import 'package:borderhacks_client/viewmodels/myappointment_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class MyAppointmentView extends StatelessWidget {
+class MyAppointmentView extends StatefulWidget {
   const MyAppointmentView({Key? key}) : super(key: key);
 
   @override
+  _MyAppointmentViewState createState() => _MyAppointmentViewState();
+}
+
+class _MyAppointmentViewState extends State<MyAppointmentView> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return BaseView<MyAppointmentViewModel>(builder: (context, model, child) {
+      return Scaffold(
+        body: _buildBody(model),
+      );
+    });
+  }
+
+  Widget _buildBody(MyAppointmentViewModel model) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
+      child: Column(
+        children: [
+          ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.symmetric(vertical: 8.h),
+            children: List<Widget>.generate(
+              2,
+              (_) => AppointmentTile(model.getDummyData()),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
